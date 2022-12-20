82ca133fe88d514bd20b0918210db58f046c8c7f
Yale Araujo <yale850@gmail.com>
2022-12-20 00:18:56 -0300

    tuplas

--- 02-basicTypes/tuple/tupleType.js
+++ 02-basicTypes/tuple/tupleType.js
@@ -1,1 +1,10 @@
 "use strict";
+let person = ['Fulano', 22, false];
+console.log(person);
+let fruitList = [1, true, 'Yale', 'Yure', 'Felipe', 'Patrícia'];
+console.log(...fruitList);
+function createPerson(...name) {
+    return [...name];
+}
+console.log(createPerson('Yale', 'Arújo', 'Santos'));
+console.log(createPerson('Yale', 'Araujo'));
---
+++ 02-basicTypes/tuple/tupleType.ts
@@ -0,0 +1,15 @@
+let person: [string, number, boolean] = ['Fulano', 22, false]
+console.log(person)
+
+let fruitList: [number, boolean, ...string[]] = [1, true, 'Yale', 'Yure', 'Felipe', 'Patrícia']
+console.log(...fruitList)
+
+type Name = | [firstName: string, lastName: string]
+    | [firstName: string, middleName: string, lastName: string]
+
+function createPerson(...name: Name) {
+    return [...name]
+}
+
+console.log(createPerson('Yale', 'Arújo', 'Santos'))
+console.log(createPerson('Yale', 'Araujo'))
