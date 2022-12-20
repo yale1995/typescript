fcf040ced1abf3b56d9b24faa7be19991fd46f26
Yale Araujo <yale850@gmail.com>
2022-12-19 23:06:18 -0300

    array

---
+++ 02-basicTypes/arrayType.js
@@ -0,0 +1,10 @@
+"use strict";
+let fruits = ['apple', 'banana', 'lemon', 'coconuts'];
+console.log(fruits);
+let cars = ['gol', 'duster oroch', 'tracker', 'a3', 'saveiro'];
+console.log(cars);
+let numberList = [0, 1, 2, 3, 4, 5];
+numberList = [...numberList, 6, 7, 8, 9];
+console.log(numberList);
+let programmingLanguages = new Array('JavaScript', 'PHP', 'Python', 'JAVA');
+console.log(programmingLanguages);
---
+++ 02-basicTypes/arrayType.ts
@@ -0,0 +1,12 @@
+let fruits: string[] = ['apple', 'banana', 'lemon', 'coconuts']
+console.log(fruits)
+
+let cars: Array<string> = ['gol', 'duster oroch', 'tracker', 'a3', 'saveiro']
+console.log(cars)
+
+let numberList: Array<number> = [0, 1, 2, 3, 4, 5]
+numberList = [...numberList, 6, 7, 8, 9]
+console.log(numberList)
+
+let programmingLanguages: string[] = new Array('JavaScript', 'PHP', 'Python', 'JAVA')
+console.log(programmingLanguages)
