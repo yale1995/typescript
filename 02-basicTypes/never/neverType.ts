24dcf82057d1f1652f05f0a3d1249554d2488ebc
Yale Araujo <yale850@gmail.com>
2022-12-21 09:54:33 -0300

    never

---
+++ 02-basicTypes/never/neverType.ts
@@ -0,0 +1,21 @@
+function error(message: string): never {
+    throw new Error(message)
+}
+console.log(error('Erro de mensagem 01'))
+
+function regectedMessage() {
+    return error('Error message 02')
+}
+
+console.log(regectedMessage())
+
+const loopVariable = function loop() {
+    while(true) {
+        console.log('hi developers')
+    }
+}
+
+console.log(loopVariable())
+
+// const variableNever:never = null => error
+const variableVoid:void = null
