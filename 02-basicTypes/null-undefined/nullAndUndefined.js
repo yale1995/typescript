f7a097eecec17cd3731cb6a3aeec832326537b33
Yale Araujo <yale850@gmail.com>
2022-12-21 08:21:58 -0300

    null and undefined

---
+++ 02-basicTypes/null-undefined/nullAndUndefined.js
@@ -0,0 +1,9 @@
+"use strict";
+let testVariableNull = null;
+console.log(testVariableNull);
+console.log(typeof testVariableNull);
+let testVariableUndefined = undefined;
+console.log(testVariableUndefined);
+console.log(typeof testVariableUndefined);
+console.log('Example 01 ', null == undefined);
+console.log('Example 02 ', null === undefined);
