6ae2c9d80c874a788089e1ca9de197e0bd33930b
Yale Araujo <yale850@gmail.com>
2022-12-26 13:02:18 -0300

    heritage

--- 05-classes/heritage/heritage.js
+++ 05-classes/heritage/heritage.js
@@ -1,16 +1,25 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 class Animal {
+    constructor(name) {
+        this.name = name;
+    }
     get walk() {
         return 'walking';
     }
 }
 class Dog extends Animal {
+    constructor(age, name) {
+        super(name);
+        this.age = age;
+    }
     get sound() {
         return 'au au';
     }
 }
-const newAnimal = new Dog();
+const newAnimal = new Dog(11, 'Theo');
 console.log(newAnimal);
 console.log(newAnimal.sound);
 console.log(newAnimal.walk);
+console.log(newAnimal.age);
+console.log(newAnimal.name);
--- 05-classes/heritage/heritage.ts
+++ 05-classes/heritage/heritage.ts
@@ -1,19 +1,31 @@
-export {}
+export { }
 
 class Animal {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name
+    }
     get walk() {
         return 'walking'
     }
 }
 
 class Dog extends Animal {
+    age: number;
+    constructor(age: number, name: string) {
+        super(name)
+        this.age = age
+    }
     get sound() {
         return 'au au'
     }
 }
 
-const newAnimal = new Dog()
+const newAnimal = new Dog(11, 'Theo')
 
 console.log(newAnimal)
 console.log(newAnimal.sound)
 console.log(newAnimal.walk)
+console.log(newAnimal.age)
+console.log(newAnimal.name)
